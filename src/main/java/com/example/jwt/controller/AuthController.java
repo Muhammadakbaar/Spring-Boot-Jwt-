496b5a0a1f346ca7eb32a9ba754f30ae8d0b48fd
zaini24 <zainibanget@gmail.com>
2020-09-14 10:56:36 +0700

    spring boot jwt

---
+++ src/main/java/com/example/jwt/controller/AuthController.java
@@ -0,0 +1,123 @@
+package com.example.jwt.controller;
+
+import com.example.jwt.exception.AppException;
+import com.example.jwt.model.Role;
+import com.example.jwt.model.RoleName;
+import com.example.jwt.model.User;
+import com.example.jwt.payload.ApiResponse;
+import com.example.jwt.payload.JwtAuthenticationResponse;
+import com.example.jwt.payload.LoginRequest;
+import com.example.jwt.payload.SignUpRequest;
+import com.example.jwt.repository.RoleRepository;
+import com.example.jwt.repository.UserRepository;
+import com.example.jwt.security.JwtAuthenticationFilter;
+import com.example.jwt.security.JwtTokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Autowired
+    JwtTokenProvider tokenProvider;
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsernameOrEmail(),
+                        loginRequest.getPassword()
+                )
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt = tokenProvider.generateToken(authentication);
+        return ResponseEntity.ok(new JwtAuthenticationResponse(jwt));
+    }
+
+    private String getJwt(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7, bearerToken.length());
+        }
+
+        return null;
+    }
+
+    @GetMapping("/user")
+    public ResponseEntity<?> user(HttpServletRequest request) {
+        String bearerToken = this.getJwt(request);
+        if (bearerToken == null) {
+            return ResponseEntity.status(403).body("akses tidak diizinkan.");
+        }
+
+        long userId = tokenProvider.getUserIdFromJWT(bearerToken);
+        Optional<User> user = userRepository.findById(userId);
+
+        return ResponseEntity.ok(user);
+    }
+
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody SignUpRequest signUpRequest) {
+        if(userRepository.existsByUsername(signUpRequest.getUsername())) {
+            return new ResponseEntity(new ApiResponse(false, "Username is already taken!"),
+                    HttpStatus.BAD_REQUEST);
+        }
+
+        if(userRepository.existsByEmail(signUpRequest.getEmail())) {
+            return new ResponseEntity(new ApiResponse(false, "Email Address already in use!"),
+                    HttpStatus.BAD_REQUEST);
+        }
+
+        // Creating user's account
+        User user = new User(signUpRequest.getName(), signUpRequest.getUsername(),
+                signUpRequest.getEmail(), signUpRequest.getPassword());
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        Role userRole = roleRepository.findByName(RoleName.ROLE_USER)
+                .orElseThrow(() -> new AppException("User Role not set."));
+
+        user.setRoles(Collections.singleton(userRole));
+
+        User result = userRepository.save(user);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentContextPath().path("/api/users/{username}")
+                .buildAndExpand(result.getUsername()).toUri();
+
+        return ResponseEntity.created(location).body(new ApiResponse(true, "User registered successfully"));
+    }
+}
---
+++ src/main/java/com/example/jwt/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.example.jwt.repository;
+
+import com.example.jwt.model.Role;
+import com.example.jwt.model.RoleName;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(RoleName roleName);
+}
+
